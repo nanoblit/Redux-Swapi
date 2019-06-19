59e079db6cced7750bffb648c8af5b8b72a28107
nanoblit <nanoblit@users.noreply.github.com>
2019-06-19 15:35:57 +0200

    Finish MVP

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,22 @@
 import axios from 'axios';
 
-export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+export const SET_CHARACTERS = 'FETCH_CHARACTERS';
+export const SET_FETCHING = 'SET_FETCHING';
 export const FETCHING_SUCCESS = 'FETCHING_SUCCESS';
 export const FETCHING_FAILURE = 'FETCHING_FAILURE';
 // we'll need to create 3 different action types here.
 // one for fetching, one for success and one for failure
 
+export const fetchCharacters = () => dispatch => {
+  dispatch({ type: SET_FETCHING });
+  axios
+    .get('https://swapi.co/api/people/')
+    .then(res => {
+      dispatch({ type: SET_CHARACTERS, payload: res.data.results });
+      dispatch({ type: FETCHING_SUCCESS });
+    })
+    .catch(() => dispatch({ type: FETCHING_FAILURE }));
+};
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -1,16 +1,22 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import {
-  shape, arrayOf, string, bool,
+  shape, arrayOf, string, bool, func,
 } from 'prop-types';
 
 import { CharacterList } from '../components';
-// import actions
+import { fetchCharacters as fetchCharactersAction } from '../actions';
 
-const CharacterListView = ({ fetching, characters }) => {
-  useEffect(() => {}, []);
+const CharacterListView = ({
+  fetchCharacters, fetching, fetchingFailed, characters,
+}) => {
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
 
-  if (fetching) {
+  if (fetchingFailed) {
+    return <div>Couldn&apos;t fetch characters</div>;
+  } if (fetching) {
     return <div>Fetching characters...</div>;
   }
   return (
@@ -21,17 +27,23 @@ const CharacterListView = ({ fetching, characters }) => {
 };
 
 CharacterListView.propTypes = {
+  fetchCharacters: func.isRequired,
   characters: arrayOf(shape({ name: string })).isRequired,
   fetching: bool.isRequired,
+  fetchingFailed: bool.isRequired,
 };
 
-const mapStateToProps = state => ({ fetching: state.fetching, characters: state.characters });
+const mapStateToProps = state => ({
+  fetching: state.fetching,
+  characters: state.characters,
+  fetchingFailed: state.fetchingFailed,
+});
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
   mapStateToProps,
   {
-    /* action creators go here */
+    fetchCharacters: fetchCharactersAction,
   },
 )(CharacterListView);
